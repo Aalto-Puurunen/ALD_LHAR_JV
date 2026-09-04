2001ae0abae097fca2b0d0ddccc3bcbd95af8b38
jorgevelasco7 <125366012+jorgevelasco7@users.noreply.github.com>
2026-09-04 16:39:37 +0300

    Update ALD_LHAR_JV_v1.8.1.1.py

--- Script ALD_LHAR_JV_v1.8.1.1/ALD_LHAR_JV_v1.8.1.1.py
+++ Script ALD_LHAR_JV_v1.8.1.1/ALD_LHAR_JV_v1.8.1.1.py
@@ -1,6 +1,6 @@
 """
 ALD_LHAR_JV
-Diffusion-reaction model for ALD on a rectangular lateral high-aspect-ratio (LHAR) cavity. 
+Diffusion-reaction model for ALD on a rectangular lateral high-aspect-ratio (LHAR) cavity 
 Version: 1.8.1.1
 September 4th, 2026
 
